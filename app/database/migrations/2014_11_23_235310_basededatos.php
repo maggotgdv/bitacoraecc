e6df8b059ab59f25ba62b3ebded34e28c255bb62
Ernesto <ecalderoncc@gmail.com>
2014-11-23 19:14:00 -0600

    Commit 1
    Primer commit de la base de datos

--- app/database/migrations/2014_11_23_235310_basededatos.php
+++ app/database/migrations/2014_11_23_235310_basededatos.php
@@ -12,7 +12,48 @@ class Basededatos extends Migration {
 	 */
 	public function up()
 	{
-		//
+		Schema::create('T001USUARIO', function($table)
+		{
+			$table->incremental('id');
+		    $table->string('nomusu',45);
+		    $table->string('apllusu',45);
+		    $table->unsignedInteger('tipousu');
+		    $table->string('carrusu',45)	
+		    $table->boolean('aptousu',45)
+			$table->integer('matusu');
+		});
+
+		Schema::create('T002ACTIVIDAD', function($table)
+		{
+			$table->incremental('id');
+		    $table->string('nomact',45);
+		    $table->date('fechact',45);
+		    $table->integer('costoact');
+		    $table->string('descact',45)	
+		    $table->string('evidenact',200)
+			$table->boolean('programact');
+			$table->string('respact',45);
+		});
+
+		Schema::create('T003BITACORA', function($table)
+		{
+			$table->incremental('id');
+		  	$table->unsignedInteger('codact');
+     			$table->foreign('codact')->references('id')->on('T002ACTIVIDAD');
+		  	$table->unsignedInteger('codusu');
+     			$table->foreign('codusu')->references('id')->on('T001USUARIO');
+		});
+
+		Schema::create('T004USUARIO_ADMIN', function($table)
+		{
+			$table->incremental('id');
+		    $table->string('user',45);
+		    $table->string('passw',45);
+		    $table->string('nomusu',45);
+		    $table->string('apllusu',45);
+		    $table->integer('tipousu');
+		});
+
 	}
 
 	/**
@@ -22,7 +63,10 @@ class Basededatos extends Migration {
 	 */
 	public function down()
 	{
-		//
+		Schema::drop('T001USUARIO');
+		Schema::drop('T002ACTIVIDAD');
+		Schema::drop('T003BITACORA');
+		Schema::drop('T004USUARIO_ADMIN');
 	}
 
 }
