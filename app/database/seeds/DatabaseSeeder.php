9fbd5ec57bd76d914ebb5e333be26295f4c22bec
Ernesto <ecalderoncc@gmail.com>
2014-11-24 03:15:56 -0600

    Login del Usuario

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder {
 		Eloquent::unguard();
 
 		//Usuario
-		DB::table('categorias')->insert(array('nomusu'=>'Ernesto', 'apllusu'=>'Calderon', 'tipousu'=>1,'carrusu'=> 'Ingenieria de Software', 'aptousu'=>true,'matusu'=>'2010200521'));
-		DB::table('categorias')->insert(array('nomusu'=>'Juan', 'apllusu'=>'Tragocamote', 'tipousu'=>1,'carrusu'=> 'Medicina', 'aptousu'=>false,'matusu'=>'2012224589'));
+		DB::table('T001USUARIO')->insert(array('nomusu'=>'Ernesto', 'apllusu'=>'Calderon', 'tipousu'=>1,'carrusu'=> 'Ingenieria de Software', 'aptousu'=>true,'matusu'=>'2010200521'));
+		DB::table('T001USUARIO')->insert(array('nomusu'=>'Juan', 'apllusu'=>'Tragocamote', 'tipousu'=>1,'carrusu'=> 'Medicina', 'aptousu'=>false,'matusu'=>'2012224589'));
 		
 	}
 
